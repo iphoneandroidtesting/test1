afc2cc33865405c8098a1f67176fcb82c89a30dd
ivan0x32 <ivan0x32@gmail.com>
2014-02-03 15:58:59 +0200

    Fixed api url bug (missing slash in the end of "/api/v2")  in Config class

--- src/com/nmotion/android/utils/Config.java
+++ src/com/nmotion/android/utils/Config.java
@@ -25,7 +25,7 @@ public class Config {
 	
 	// prod
     public static final String APP_ID = SERVER_INDEX==2 ? "132227170295952" : (SERVER_INDEX==1 ? "330287150421549" :"124395924385734");
-    public static final String SERVICE_URI_API_PART = "/api/v2";
+    public static final String SERVICE_URI_API_PART = "/api/v2/";
     public static final String SERVICE_URL = SERVER_INDEX==2 ? "http://nmotion.dk" : "http://79.143.37.62:8088";
     public static final String SERVICE_URI = SERVICE_URL+SERVICE_URI_API_PART;
     public static final String ACCEPT_URL_CALLBACK = SERVICE_URL+"/paymentconfirmation/accepted";
